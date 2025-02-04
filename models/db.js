5c9c80ca2486f6eb8b1001c1911cbad7d1a4d0f9
schasipanta <sthalin@gmail.com>
2025-02-04 13:06:29 -0500

    2

--- models/db.js
+++ models/db.js
@@ -1,5 +1,27 @@
 import mongoose from "mongoose";
 
+const cloudURI = "mongodb+srv://fabian:1234@example.gq0uy.mongodb.net/";
+
+const connectDB = async () => {
+  try {
+    await mongoose.connect(cloudURI, {
+      useNewUrlParser: true,
+      useUnifiedTopology: true,
+    });
+    console.log("Conexión exitosa a MongoDB en la nube");
+  } catch (error) {
+    console.error("Error conectando a MongoDB en la nube:", error.message);
+    process.exit(1); // Termina el proceso en caso de error
+  }
+};
+
+// Exportación por defecto
+export default connectDB;
+
+
+/*
+import mongoose from "mongoose";
+
 const connectDB = async () => {
   try {
     await mongoose.connect("mongodb://127.0.0.1:27017/loginDB", {
@@ -15,3 +37,4 @@ const connectDB = async () => {
 
 // Exportación por defecto
 export default connectDB;
+*/
